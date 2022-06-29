d816cb7828af1d344bd31f6dcd3208879ddfcb9b
msatyam566 <msatyam566@gmail.com>
2022-06-29 13:04:08 +0530

    updated

---
+++ WeatherApp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Temp from"./component/weatherApp/temp"
+
+const App = () => {
+  return (
+    <>
+      <Temp />
+    </>
+  );
+};
+
+export default App;
